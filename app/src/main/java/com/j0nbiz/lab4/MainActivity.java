e727cb857b73584200324cbd2ea288d4c5a72004
Jonathan Bizier <j0nbiz@outlook.com>
2016-09-19 14:10:52 -0400

    Initial commit

---
+++ app/src/main/java/com/j0nbiz/lab4/MainActivity.java
@@ -0,0 +1,30 @@
+package com.j0nbiz.lab4;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showMap(View v) {
+        Uri geoLocation = Uri.parse("geo:0,0?q=" + getResources().getString(R.string.lbl_title));
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+}
